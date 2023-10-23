efb1dc063536ac715793b1d69f5548db1a7b1c7f
zeanzai <zeanzai.me@gmail.com>
2023-10-23 09:26:03 +0000

    Deploying to gh-pages from @ zeanzai/zeanzai.github.io@426aa3485ead94e9662589b84071f44dce4e14cd 🚀

---
+++ assets/docker.html.2265c9a3.js
@@ -0,0 +1,116 @@
+import{e as n}from"./app.77a131fa.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const e={},a=n(`<div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>// \u5B89\u88C5yum\u6E90\u7684\u5DE5\u5177\u5305
+yum install -y yum-utils
+
+// \u914D\u7F6Edocker\u7684\u5B89\u88C5\u6E90
+yum-config-manager \\
+--add-repo \\
+https://download.docker.com/linux/centos/docker-ce.repo
+
+// \u5B89\u88C5docker
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+// \u8BBE\u7F6E\u5F00\u673A\u81EA\u542F
+systemctl enable docker
+
+
+// \u914D\u7F6E\u963F\u91CC\u4E91\u7684\u955C\u50CF\u6E90\uFF0C\u5E2E\u52A9\u6587\u6863\uFF1A https://cr.console.aliyun.com/cn-hangzhou/instances/mirrors
+mkdir -p /etc/docker
+tee /etc/docker/daemon.json &lt;&lt;-&#39;EOF&#39;
+{
+&quot;registry-mirrors&quot;: [&quot;https://hpifphoh.mirror.aliyuncs.com&quot;]
+}
+EOF
+systemctl daemon-reload
+systemctl restart docker
+
+// \u67E5\u770B\u5B89\u88C5\u4E4B\u540E\u7684docker\u4FE1\u606F\uFF0C\u6700\u540E\u53EF\u4EE5\u770B\u5230\u914D\u7F6E\u7684\u963F\u91CC\u4E91\u7684\u955C\u50CF\u6E90
+[root@home \uFF5E]# docker info
+Client: Docker Engine - Community
+ Version:    24.0.6
+ Context:    default
+ Debug Mode: false
+ Plugins:
+  buildx: Docker Buildx (Docker Inc.)
+    Version:  v0.11.2
+    Path:     /usr/libexec/docker/cli-plugins/docker-buildx
+  compose: Docker Compose (Docker Inc.)
+    Version:  v2.21.0
+    Path:     /usr/libexec/docker/cli-plugins/docker-compose
+
+Server:
+ Containers: 0
+  Running: 0
+  Paused: 0
+  Stopped: 0
+ Images: 0
+ Server Version: 24.0.6
+ Storage Driver: overlay2
+  Backing Filesystem: xfs
+  Supports d_type: true
+  Using metacopy: false
+  Native Overlay Diff: true
+  userxattr: false
+ Logging Driver: json-file
+ Cgroup Driver: cgroupfs
+ Cgroup Version: 1
+ Plugins:
+  Volume: local
+  Network: bridge host ipvlan macvlan null overlay
+  Log: awslogs fluentd gcplogs gelf journald json-file local logentries splunk syslog
+ Swarm: inactive
+ Runtimes: io.containerd.runc.v2 runc
+ Default Runtime: runc
+ Init Binary: docker-init
+ containerd version: 61f9fd88f79f081d64d6fa3bb1a0dc71ec870523
+ runc version: v1.1.9-0-gccaecfc
+ init version: de40ad0
+ Security Options:
+  seccomp
+   Profile: builtin
+ Kernel Version: 3.10.0-1160.71.1.el7.x86_64
+ Operating System: CentOS Linux 7 (Core)
+ OSType: linux
+ Architecture: x86_64
+ CPUs: 8
+ Total Memory: 23.26GiB
+ Name: home.centos
+ ID: bf0036ec-e56a-4c78-ae07-d8e224f11480
+ Docker Root Dir: /var/lib/docker
+ Debug Mode: false
+ Experimental: false
+ Insecure Registries:
+  127.0.0.0/8
+ Registry Mirrors:
+  https://hpifphoh.mirror.aliyuncs.com/
+ Live Restore Enabled: false
+
+// \u67E5\u770Bdocker\u7684\u7248\u672C\u4FE1\u606F
+[root@home ~]# docker version
+Client: Docker Engine - Community
+ Version:           24.0.6
+ API version:       1.43
+ Go version:        go1.20.7
+ Git commit:        ed223bc
+ Built:             Mon Sep  4 12:35:25 2023
+ OS/Arch:           linux/amd64
+ Context:           default
+
+Server: Docker Engine - Community
+ Engine:
+  Version:          24.0.6
+  API version:      1.43 (minimum version 1.12)
+  Go version:       go1.20.7
+  Git commit:       1a79695
+  Built:            Mon Sep  4 12:34:28 2023
+  OS/Arch:          linux/amd64
+  Experimental:     false
+ containerd:
+  Version:          1.6.24
+  GitCommit:        61f9fd88f79f081d64d6fa3bb1a0dc71ec870523
+ runc:
+  Version:          1.1.9
+  GitCommit:        v1.1.9-0-gccaecfc
+ docker-init:
+  Version:          0.19.0
+  GitCommit:        de40ad0
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br><span class="line-number">25</span><br><span class="line-number">26</span><br><span class="line-number">27</span><br><span class="line-number">28</span><br><span class="line-number">29</span><br><span class="line-number">30</span><br><span class="line-number">31</span><br><span class="line-number">32</span><br><span class="line-number">33</span><br><span class="line-number">34</span><br><span class="line-number">35</span><br><span class="line-number">36</span><br><span class="line-number">37</span><br><span class="line-number">38</span><br><span class="line-number">39</span><br><span class="line-number">40</span><br><span class="line-number">41</span><br><span class="line-number">42</span><br><span class="line-number">43</span><br><span class="line-number">44</span><br><span class="line-number">45</span><br><span class="line-number">46</span><br><span class="line-number">47</span><br><span class="line-number">48</span><br><span class="line-number">49</span><br><span class="line-number">50</span><br><span class="line-number">51</span><br><span class="line-number">52</span><br><span class="line-number">53</span><br><span class="line-number">54</span><br><span class="line-number">55</span><br><span class="line-number">56</span><br><span class="line-number">57</span><br><span class="line-number">58</span><br><span class="line-number">59</span><br><span class="line-number">60</span><br><span class="line-number">61</span><br><span class="line-number">62</span><br><span class="line-number">63</span><br><span class="line-number">64</span><br><span class="line-number">65</span><br><span class="line-number">66</span><br><span class="line-number">67</span><br><span class="line-number">68</span><br><span class="line-number">69</span><br><span class="line-number">70</span><br><span class="line-number">71</span><br><span class="line-number">72</span><br><span class="line-number">73</span><br><span class="line-number">74</span><br><span class="line-number">75</span><br><span class="line-number">76</span><br><span class="line-number">77</span><br><span class="line-number">78</span><br><span class="line-number">79</span><br><span class="line-number">80</span><br><span class="line-number">81</span><br><span class="line-number">82</span><br><span class="line-number">83</span><br><span class="line-number">84</span><br><span class="line-number">85</span><br><span class="line-number">86</span><br><span class="line-number">87</span><br><span class="line-number">88</span><br><span class="line-number">89</span><br><span class="line-number">90</span><br><span class="line-number">91</span><br><span class="line-number">92</span><br><span class="line-number">93</span><br><span class="line-number">94</span><br><span class="line-number">95</span><br><span class="line-number">96</span><br><span class="line-number">97</span><br><span class="line-number">98</span><br><span class="line-number">99</span><br><span class="line-number">100</span><br><span class="line-number">101</span><br><span class="line-number">102</span><br><span class="line-number">103</span><br><span class="line-number">104</span><br><span class="line-number">105</span><br><span class="line-number">106</span><br><span class="line-number">107</span><br><span class="line-number">108</span><br><span class="line-number">109</span><br><span class="line-number">110</span><br><span class="line-number">111</span><br><span class="line-number">112</span><br><span class="line-number">113</span><br><span class="line-number">114</span><br><span class="line-number">115</span><br></div></div><hr><p><br><br><br><img style="border:1px red solid;display:block;margin:0 auto;" src="https://tianqingxiaozhu.oss-cn-shenzhen.aliyuncs.com/img/qrcode.jpg" alt="\u5FAE\u4FE1\u516C\u4F17\u53F7"></p>`,3);function r(l,p){return a}var c=s(e,[["render",r],["__file","docker.html.vue"]]);export{c as default};
---
+++ assets/redis.html.e5c3722f.js
@@ -0,0 +1,10 @@
+import{e}from"./app.77a131fa.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const n={},r=e(`<div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>
+[root@home blobs]# docker pull redis
+
+[root@home blobs]# docker run -p 6379:6379 --name redis \\
+&gt; -v /mnt/doc/docker/redis/data:/data \\
+&gt; -v /mnt/doc/docker/redis/conf/redis.conf:/etc/redis/redis.conf \\
+&gt; -d redis redis-server /etc/redis/redis.conf
+9de5bf8fe6ddc8b4cb8df61684ddd1613ff3944e5664d1cdb87c9192465eb42e
+
+</code></pre><div class="line-numbers" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br></div></div><p><br><br><br><img style="border:1px red solid;display:block;margin:0 auto;" src="https://tianqingxiaozhu.oss-cn-shenzhen.aliyuncs.com/img/qrcode.jpg" alt="\u5FAE\u4FE1\u516C\u4F17\u53F7"></p>`,2);function a(d,c){return r}var o=s(n,[["render",a],["__file","redis.html.vue"]]);export{o as default};
